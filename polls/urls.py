04497dca3ddb8421a5fc4775dc2c60e010ee23a2
seed.yoon <seed.yoon@kakaogames.com>
2018-11-28 15:25:22 +0900

    tool bar settings * url에 debug 상태 추가

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+from django.conf import settings
+from django.urls import path, include
+
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+                      path('__debug__/', include(debug_toolbar)),
+                  ] + urlpatterns
