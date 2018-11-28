e1903ff63975e21c3c7a40f7efbaa05325ccba3e
seed.yoon <seed.yoon@kakaogames.com>
2018-11-28 17:31:40 +0900

    기본 뷰 뼈대 생성 * 라우팅에 따라 파라메터를 전달해 줄 수 있게 설정

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /polls/
     path('', views.index, name='index'),
-]
 
+    # /polls/{num}
+    path('<int:question_id>/', views.detail, name='detail'),
+
+    # /polls/{num}/results
+    path('<int:question_id>/results/', views.results, name='results'),
+
+    # /polls/{num}/vote
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
