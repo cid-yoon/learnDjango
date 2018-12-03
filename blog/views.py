d0949e953bd040f4e2cd9ca1204ab42f8f5ee818
seed.yoon <seed.yoon@kakaogames.com>
2018-12-03 21:27:59 +0900

    댓글 추가 삭제

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 from blog.forms import PostForm, CommentForm
-from .models import Post
+from .models import Post, Comment
 
 
 # Create your views here.
@@ -84,8 +84,22 @@ def add_comment_to_post(request, pk):
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
-            return redirect('blog:post_detail')
+            return redirect('blog:post_detail', pk)
     else:
         form = CommentForm()
 
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('blog:post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('blog:post_detail', pk=comment.post.pk)
