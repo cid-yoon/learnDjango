c2677c301bba4bb3d6f15826661e71c4966d9a46
seed.yoon <seed.yoon@kakaogames.com>
2018-12-03 18:29:01 +0900

    게시 되지 않은 글 보기 게시 되지 않은 글 퍼블리시 하기

--- blog/urls.py
+++ blog/urls.py
@@ -18,6 +18,9 @@ urlpatterns = [
     path('post/edit/<int:pk>', views.post_edit, name='post_edit'),
 
     # /blog/drafts
-    path('post/drafts/', views.post_draft_list, name='post_draft_list')
+    path('post/drafts/', views.post_draft_list, name='post_draft_list'),
+
+    #
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
 
 ]
