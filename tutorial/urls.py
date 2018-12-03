0626156eca3948e3a862b96ebb21567f94330af4
seed.yoon <seed.yoon@kakaogames.com>
2018-12-03 19:09:50 +0900

    새로 변경된 템플릿 테스트 * 템플릿 추가 + 경로 설정

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
     path('polls/', include('polls.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('admin/', admin.site.urls),
 ]
